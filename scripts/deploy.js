d2267007fbbb2546b0595b62c5ab825dcd008390
diana eremina <diana.eremina@virtualhealth.com>
2023-06-07 16:06:44 +0300

    First commit

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+
+var dotenv = require('dotenv')
+var dotenvExpand = require('dotenv-expand')
+var myEnv = dotenv.config()
+dotenvExpand.expand(myEnv)
+const hre = require("hardhat");
+
+const { API_URL } = process.env;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+async function main() {
+  const DiToken = await hre.ethers.getContractFactory("DiToken");
+  const totalSupply = '100';
+  const token = await DiToken.deploy("DiToken", "DT", web3.utils.toWei(totalSupply,"ether"));
+
+  await token.deployed();
+
+  console.log(
+    `deployed to ${token.address}`
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
